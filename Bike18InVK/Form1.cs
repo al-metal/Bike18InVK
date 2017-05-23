553999f147721b0ce205af18252ebd630b78121d
al-metal <al-metal@bk.ru>
2017-05-23 22:11:42 +0400

    Авторизация вк

--- Bike18InVK/Form1.cs
+++ Bike18InVK/Form1.cs
@@ -5,9 +5,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VkNet.Enums.Filters;
 
 namespace Bike18InVK
 {
@@ -15,6 +17,7 @@ namespace Bike18InVK
     {
         nethouse nethouse = new nethouse();
         httpRequest http = new httpRequest();
+
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +30,26 @@ namespace Bike18InVK
             Properties.Settings.Default.loginVk = tbVkLogin.Text;
             Properties.Settings.Default.passwordVk = tbVkPass.Text;
             Properties.Settings.Default.Save();
+
+            CookieContainer cookieNethouse = new CookieContainer();
+            cookieNethouse = nethouse.CookieNethouse(tbNethouseLogin.Text, tbNethousePass.Text);
+
+            if(cookieNethouse.Count != 4)
+            {
+                MessageBox.Show("Логин/пароль не верный");
+                return;
+            }
+            CookieContainer cookieVk = new CookieContainer();
+            var vk = new VkNet.VkApi();
+            var idVkProgram = 5464980;
+            Settings scope = Settings.Friends;
+            vk.Authorize(new VkNet.ApiAuthParams
+            {
+                ApplicationId = (ulong)idVkProgram,
+                Login = tbVkLogin.Text,
+                Password = tbVkPass.Text,
+                Settings = scope
+            });
         }
 
         private void Form1_Load(object sender, EventArgs e)
